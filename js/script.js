e53820b2eb4f46cbc7b2b3a20a155a07670cb92d
Rodrigo <rodguevara212007@gmail.com>
2026-09-04 08:39:14 -0600

    ENTREGABLE 3 COMPLETO

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  if (slides.length > 0) {
+    setInterval(() => {
+      slides[currentSlide].classList.remove("active");
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add("active");
+    }, 4000);
+  }
+});
